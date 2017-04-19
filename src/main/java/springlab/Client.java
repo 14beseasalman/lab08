299c2c49f140d989e0be01233f1196cc22c10460
Asad Salman <me@asad.co>
2017-04-19 15:56:32 +0500

    test case added

--- src/main/java/springlab/Client.java
+++ src/main/java/springlab/Client.java
@@ -1,8 +1,33 @@
 package springlab;
 
+import springlab.dao.CloDao;
+import springlab.entity.Clo;
+
 public class Client {
 
 	public static void main(String[] args) {
+		CloDao clodao = new CloDao();
+
+		// Add new clo
+		Clo clo = new Clo();
+		clo.setName("CLO 1");
+		clo.setDescription("Design efficient solutions for algorithmic problems");
+		clo.setPlo("2");
+		clodao.addClo(clo);
+
+		clodao.updateClo(clo);
+
+		// Delete an existing clo
+		// dao.deleteClo(1);
+
+		// Get all clos
+		for (Clo iter : clodao.getAllClos()) {
+			System.out.println(iter);
+		}
+		
+		// Get clo by id
+		System.out.println(clodao.getCloById(1));
+		System.out.println("WOOT");
 		
 
 	}
